4af234548c36bd7a9fddaa550642f130ba338d38
Joshua Schneider <dev@jshs.de>
2015-07-15 14:04:36 +0200

    Split implementation and tests

--- tests/Normal_Form.thy
+++ tests/Normal_Form.thy
@@ -1,26 +1,8 @@
 theory Normal_Form
-imports Abstract_AF
+imports "../src/Applicative" Abstract_AF
 begin
 
 section {* Normal form conversion *}
-
-subsection {* Setup *}
-
-lemma comp_def_ext:
-  "comp = (\<lambda>g f x. g (f x))"
-by rule+ simp
-
-lemma id_unfold: "id \<equiv> \<lambda>x. x"
-by (unfold id_def)
-
-lemma comp_unfold: "comp \<equiv> (\<lambda>g f x. g (f x))"
-by (unfold comp_def_ext)
-
-ML_file "applicative.ML"
-
-method_setup lifting_nf = {* Scan.succeed (fn ctxt =>
-  SIMPLE_METHOD' (Applicative.lifting_nf_tac ctxt)) *}
-
 
 subsection {* Example: Abstract functor *}
 
