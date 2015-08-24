1fa726a66facb6345d3ee6e6e70090305cafc151
Joshua Schneider <dev@jshs.de>
2015-08-24 23:47:22 +0200

    Refactoring and clean-up (registration part)

--- src/Applicative.thy
+++ src/Applicative.thy
@@ -13,10 +13,10 @@ method_setup general_lifting = {* Scan.succeed (fn ctxt =>
 
 ML {* Outer_Syntax.local_theory_to_proof @{command_keyword "applicative"}
   "register applicative functors"
-  (Scan.option Parse.name --
+  (Parse.binding --
     Scan.optional (@{keyword "("} |-- Parse.list Parse.short_ident --| @{keyword ")"}) [] --|
     @{keyword "for"} --| Parse.reserved "pure" --| @{keyword ":"} -- Parse.term --|
     Parse.reserved "ap" --| @{keyword ":"} -- Parse.term >>
-    (fn (((_, combs), pure), ap) => Applicative.applicative_cmd "" pure ap combs)) *}
+    (fn (((name, combs), pure), ap) => Applicative.applicative_cmd name pure ap combs)) *}
 
 end
