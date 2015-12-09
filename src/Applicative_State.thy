651780b8da53ebbb8d166d9e66bcd839258acc22
Andreas Lochbihler <andreas.lochbihler@inf.ethz.ch>
2015-12-09 15:11:58 +0100

    move Tree_Traversal to main folder, tune proofs

--- src/Applicative_State.thy
+++ src/Applicative_State.thy
@@ -11,11 +11,13 @@ type_synonym ('a, 's) state = "'s \<Rightarrow> 'a \<times> 's"
 
 definition "ap_state f x = (\<lambda>s. case f s of (g, s') \<Rightarrow> case x s' of (y, s'') \<Rightarrow> (g y, s''))"
 
+abbreviation (input) "pure_state \<equiv> Pair"
+
 adhoc_overloading Applicative.ap ap_state
 
 applicative state
 for
-  pure: Pair
+  pure: pure_state
   ap: "ap_state :: ('a \<Rightarrow> 'b, 's) state \<Rightarrow> ('a, 's) state \<Rightarrow> ('b, 's) state"
 unfolding ap_state_def
 by (auto split: split_split)
